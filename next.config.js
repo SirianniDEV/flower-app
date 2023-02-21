c797e071d897da8e95f6716b68218caab1d54af5
SofiaBCIT <sofia.sirianni@outlook.com>
2023-02-21 01:10:37 -0800

    trouble shooting

--- next.config.js
+++ next.config.js
@@ -17,4 +17,14 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig;
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/about',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
+}
